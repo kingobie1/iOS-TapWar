c9cbacd681f089d1f8342092af5b8cec7818eecf
Obatola Seward-Evans <oba.seward@gmail.com>
2016-07-14 08:06:19 -0700

    added dynamically sized buttons

--- TapWar/MainGameViewController.swift
+++ TapWar/MainGameViewController.swift
@@ -20,22 +20,28 @@ class MainGameViewController: UIViewController {
     @IBOutlet weak var topButton: UIButton!
     @IBOutlet weak var bottomButton: UIButton!
     
-    @IBOutlet var BottomButtonHeight: NSLayoutConstraint!
+    @IBOutlet var bottomButtonHeight: NSLayoutConstraint!
     @IBOutlet weak var topLabel: UILabel!
     @IBOutlet weak var bottomLabel: UILabel!
     
-    var bottomButtonHeightChunk: CGFloat?
+    var tugLength: CGFloat?
 
     override func viewDidLoad() {
         
+        tugLength = self.view.frame.height / CGFloat( endingScore * 2 )
+        bottomButtonHeight.constant = self.view.frame.height / 2
+        
         let topButtonColor = UIColor(netHex: 0x53ccf7)
         let bottomButtonColor = UIColor(netHex: 0xec6559)
+        let textColor = UIColor(netHex: 0xecf0f1)
         
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         resetGame()
-        
+
+        topLabel.textColor = textColor
+        bottomLabel.textColor = textColor
         topButton.backgroundColor = topButtonColor
         bottomButton.backgroundColor = bottomButtonColor
         topLabel.transform = CGAffineTransformMakeRotation(3.14)
@@ -45,18 +51,27 @@ class MainGameViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func preferredStatusBarStyle() -> UIStatusBarStyle {
+        return UIStatusBarStyle.LightContent
+    }
 
     // MARK Acitons
     
     @IBAction func topButtonAction(sender: AnyObject) {
         
-        score += 1
+        // decrease height of bottomButton for each topButton tap:
+        bottomButtonHeight.constant -= tugLength!
         
+        score += 1
         reloadScoreLabels()
         testScore()
     }
     
     @IBAction func bottomButtonAction(sender: AnyObject) {
+        
+        // increase height of bottomButton for each bottomButton tap:
+        bottomButtonHeight.constant += tugLength!
         
         score -= 1
         reloadScoreLabels()
@@ -94,6 +109,7 @@ class MainGameViewController: UIViewController {
             gameData = GameData.init(winner: Players.Bottom, loser: Players.Top)
             print("bottomWins")
             
+            
         } else {
             return
         }
@@ -104,6 +120,8 @@ class MainGameViewController: UIViewController {
     func resetGame() {
         print("Game Reset")
         
+        bottomButtonHeight.constant = self.view.frame.height / 2
+
         score = 0
         topLabel.text = "\(score)"
         bottomLabel.text = "\(score)"
